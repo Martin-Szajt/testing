d3527d389f31a997bb5346534318db37c2a2a326
Martin-Szajt <58597855+Martin-Szajt@users.noreply.github.com>
2021-02-03 19:30:12 -0300

    Update cdn.js

--- cdn.js
+++ cdn.js
@@ -1,21 +1,17 @@
-var reference = (function thename(){
+var run = (function createConnectButton(){
 
-var button = document.getElementById("cdn-button");
-  
-  
-var div = document.getElementById("cdn-button");
-var button = document.createElement('button');
-button.innerHTML = 'clickeame';
-button.style.backgroundColor = "#ff0000";
-div.appendChild(button);
-  
-var token = div.getAttribute('data-token');
-button.addEventListener("click", function() {
-  alert("aca estoy");
-  window.location.href="https://app.charthop.com/connect/?app="+token;
-});
+    var button = document.getElementById("cdn-button");  
+    var div = document.getElementById("cdn-button");
+    var button = document.createElement('button');
+    button.innerHTML = 'Connect With Charthop';
+    button.style.width = "300px";
+    button.style.height = "40px";
+    button.style.border = "none";
+    button.style.backgroundColor = "#ff0000";
+    div.appendChild(button);
+    var token = div.getAttribute('data-token');
+    button.addEventListener("click", function() {
+      window.location.href="https://app.charthop.com/connect/?app="+token;
+    });
     
-console.log("autorun");
 }());
-
-
