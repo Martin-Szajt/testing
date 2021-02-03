39ad029f09d46dce80854989935e55ad99fb85d0
Martin-Szajt <58597855+Martin-Szajt@users.noreply.github.com>
2021-02-03 18:06:01 -0300

    Update cdn.js

--- cdn.js
+++ cdn.js
@@ -1,10 +1,14 @@
 var reference = (function thename(){
 
+var element = document.getElementByClass(".button.primary")
+element.addEventListener("Click", function() {
+    console.log("click");
+});
+    
 document.addEventListener("DOMContentLoaded", function() {
     console.log("click");
 });
 console.log("autorun");
 }());
 
-reference(); //call it again
 
